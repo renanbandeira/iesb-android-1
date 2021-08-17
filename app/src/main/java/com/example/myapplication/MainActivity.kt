0815a4d096fa100dbd9aa3cecc28d6d2544b085a
Renan Bandeira <renan.bandeira@chilipiper.com>
2021-08-17 20:03:29 -0300

    Aula 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,33 +1,46 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.RelativeLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
-import androidx.core.view.accessibility.AccessibilityEventCompat
 import com.google.android.material.snackbar.Snackbar
-import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
-import com.example.myapplication.listeners.SubmitLoginClickListener
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var rootView: View
+
+    var pressedBackButtonTimes = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_constraint)
+
+        rootView = findViewById<ConstraintLayout>(R.id.root)
     }
 
     override fun onResume() {
         super.onResume()
-        var root = findViewById<RelativeLayout>(R.id.root)
-        Snackbar.make(root, "Activity criada", Snackbar.LENGTH_SHORT).show()
-        findViewById<Button>(R.id.submit).setOnClickListener(SubmitLoginClickListener(root, this))
+        Toast.makeText(this, "Activity entrou no onResume", Toast.LENGTH_LONG).show()
+        val button = findViewById<Button>(R.id.submit)
+        button.setOnClickListener(View.OnClickListener { view ->
+            Snackbar.make(button, "Botão clicado!", Snackbar.LENGTH_SHORT)
+                .setAction("OK", View.OnClickListener {
+                    Snackbar.make(rootView, "OK clicado", Snackbar.LENGTH_SHORT).show()
+                }).show()
+        })
     }
 
     override fun onBackPressed() {
-        Toast.makeText(this, "Botão voltar clicado", Toast.LENGTH_SHORT).show()
-        super.onBackPressed()
+        if (pressedBackButtonTimes == 0) {
+            pressedBackButtonTimes++
+            Toast.makeText(this, "Aperte novamente para sair da app", Toast.LENGTH_SHORT).show()
+        } else {
+            super.onBackPressed()
+        }
+
     }
 }
